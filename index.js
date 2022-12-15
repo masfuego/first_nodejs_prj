e8420eebb99270a97bc03612e8e3ce7ee788af3e
masfuego <morrisongrandon@gmail.com>
2022-12-15 12:12:07 +0000

    Create a page with NodeJs as a server

---
+++ index.js
@@ -0,0 +1,16 @@
+// http.createServer(function (req, res) {
+//   res.write('Hello World!');
+//   res.end(); 
+// }).listen(8080); 
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+
+  fs.readFile('home.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
